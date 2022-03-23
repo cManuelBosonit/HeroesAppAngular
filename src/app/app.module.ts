749a2210431161f0fcf0146ad4bae02d725a398c
Manuel Hernández <carlosmanuel.hernandez@bosonit.com>
2022-03-23 17:51:52 +0100

    Rutas hijas y LazyLoad

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ErrorPageComponent } from './shared/error-page/error-page.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HeroesRoutingModule } from './heroes/heroes-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ErrorPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    HeroesRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]

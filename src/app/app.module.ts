87ffded0be813dddba2b2ad291714896af3b61a6
Manuel Hernández <carlosmanuel.hernandez@bosonit.com>
2022-03-24 16:52:56 +0100

    Angular Material & Angular Flex-Layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+
 import { ErrorPageComponent } from './shared/error-page/error-page.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HeroesRoutingModule } from './heroes/heroes-routing.module';
@@ -15,6 +17,7 @@ import { HeroesRoutingModule } from './heroes/heroes-routing.module';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    HttpClientModule,
     AppRoutingModule,
     HeroesRoutingModule
   ],
